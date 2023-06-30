c8da90f083ba6be54f2521c863734b54656e244e
Sam Adams <sam.adams7785@gmail.com>
2023-06-30 11:55:18 -0700

    added restock form with different types, displaying topping dynamicly instead of static

--- src/Components/BobaController.js
+++ src/Components/BobaController.js
@@ -10,9 +10,20 @@ function BobaController() {
   const [totalPudding, setTotalPudding] = useState(36);
   const [totalRedBean, setTotalRedBean] = useState(40);
 
-  const restockBoba = () => {
-    const updatedBobaCount = totalPearl + 130;
-    setTotalPearl(updatedBobaCount);
+  const restock = (type) => {
+    if (type === "pearl") {
+      const updatedCount = totalPearl + 130;
+      setTotalPearl(updatedCount);
+    } else if (type === "lychee") {
+      const updatedCount = totalLychee + 130;
+      setTotalLychee(updatedCount);
+    } else if (type === "pudding") {
+      const updatedCount = totalPudding + 130;
+      setTotalPudding(updatedCount);
+    } else if (type === "red bean") {
+      const updatedCount = totalRedBean + 130;
+      setTotalRedBean(updatedCount);
+    }
   };
 
   const sellDrink = (drinkName) => {
@@ -38,32 +49,66 @@ function BobaController() {
 
   const [drinks, setDrinks] = useState([
     {
-      name: "Thai Pearl Milk Tea",
+      name: "Thai Milk Tea",
       price: 4.99,
       topping: "pearl",
       description:
         "Sweet and creamy milk tea that has unique thai black tea flavor creating nice strong tea taste",
     },
     {
-      name: "Classic Pearl Milk Tea",
+      name: "Classic Milk Tea",
       price: 4.99,
       topping: "pearl",
-      description:
-        "Rich and creamy milk tea with the right balance of sweetness",
+      description: "Creamy milk tea with the right balance of sweetness",
     },
     {
-      name: "Mango Green Milk Tea",
+      name: "Mango Green Tea",
       price: 4.99,
       topping: "pearl",
       description:
         "Mix of freshly brewed green tea and creamer that is sweetened with fruity mango jam creating a vibrant yellow color",
     },
     {
-      name: "Okinagwa Green Milk Tea",
-      price: 4.99,
+      name: "Okinawa Tea",
+      price: 5.99,
       topping: "pearl",
       description:
-        "Mix of freshly brewed green tea and creamer that is sweetened with fruity mango jam creating a vibrant yellow color",
+        "Okinawa tea infused with the delightful chewiness of pearl toppings",
+    },
+    {
+      name: "Peach Tea Ice Blended",
+      price: 7.99,
+      topping: "lychee",
+      description:
+        "Refreshing peach tea blended with ice and topped with delightful lychee jelly, creating a perfect balance of fruity flavors and coolness.",
+    },
+    {
+      name: "Hokkaido Milk Tea",
+      price: 5.99,
+      topping: "lychee",
+      description:
+        "Rich flavor of Hokkaido milk tea infused with the delicate sweetness of lychee jelly.",
+    },
+    {
+      name: "Taro Ice Blended",
+      price: 6.99,
+      topping: "pudding",
+      description:
+        "Distinctive flavor of taro in this refreshing ice blended drink, enhanced with the delectable addition of smooth and sweet pudding.",
+    },
+    {
+      name: "Strawberry Tea",
+      price: 6.99,
+      topping: "pudding",
+      description:
+        "Fruity Strawberry Tea mixed with the delectable addition of smooth and sweet pudding.",
+    },
+    {
+      name: "Matcha Milk Tea",
+      price: 5.99,
+      topping: "redBean",
+      description:
+        "Matcha Milk tea with premium matcha powder added gives the bubble tea a subtle bitterness that pairs perfectly with sweet red bean",
     },
   ]);
 
@@ -76,7 +121,7 @@ function BobaController() {
         totalRedBean={totalRedBean}
       />
       <BobaList drinks={drinks} sellDrink={sellDrink} />
-      <BobaForm restockBoba={restockBoba} />
+      <BobaForm restock={restock} />
       <BobaDetail />
     </React.Fragment>
   );
--- src/Components/BobaForm.js
+++ src/Components/BobaForm.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BobaForm({ restockBoba }) {
+function BobaForm({ restock }) {
+  const [selectedTopping, setSelectedTopping] = useState("");
+
+  const handleDropdownChange = (event) => {
+    setSelectedTopping(event.target.value);
+  };
+
   return (
     <React.Fragment>
-      <div className="flex justify-center items-center">
-        <button onClick={restockBoba} className="btn btn-accent btn-wide mt-4">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <select
+          value={selectedTopping}
+          onChange={handleDropdownChange}
+          className="select w-64 mt-4 outline outline-accent focus:outline-pink-300"
+        >
+          <option value="">Select Topping</option>
+          <option value="pearl">Pearl</option>
+          <option value="lychee">Lychee</option>
+          <option value="pudding">Pudding</option>
+          <option value="red bean">Red Bean</option>
+        </select>
+        <button
+          onClick={() => restock(selectedTopping)}
+          className="btn btn-accent btn-wide mt-4"
+        >
           Restock
         </button>
       </div>
